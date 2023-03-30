1b6bd7792f05f79fdfe7aae01d190f1043ec1adf
Oleg <grammy1488@gmail.com>
2023-03-30 21:24:54 +0300

    Исправление JS

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 // userPass = userPass.toLocaleLowerCase();
 
-// if (userPass === "скайпро") {
+// if (userPass === "userpass") {
 //   console.log("Пароль введен верно");
 // } else {
 //   console.log("Пароль введен не верно");
@@ -32,45 +32,31 @@
 
 // let monthNumber = String(prompt("Какой месяц?"));
 
+// let monthNumber = Number(prompt('Какой месяц?'));
+
 // switch (monthNumber) {
-//   case "1":
-//     console.log("зима");
-//     break;
-//   case "2":
-//     console.log("зима");
-//     break;
-//   case "3":
-//     console.log("весна");
-//   case "4":
-//     console.log("весна");
-//     break;
-//   case "5":
-//     console.log("весна");
-//     break;
-//   case "6":
-//     console.log("лето");
-//     break;
-//   case "7":
-//     console.log("лето");
-//     break;
-//   case "8":
-//     console.log("лето");
-//     break;
-//   case "9":
-//     console.log("осень");
-//     break;
-//   case "10":
-//     console.log("осень");
-//     break;
-//   case "11":
-//     console.log("осень");
-//     break;
-//   case "12":
-//     console.log("зима");
-//     break;
-//   default:
-//     console.log("Устал считать");
-//     break;
-// }
+//     case 12:
+//     case 1:
+//     case 2:
+//       console.log('зима');
+//       break;
+//     case 3:
+//     case 4:
+//     case 5:
+//       console.log('весна');
+//       break;
+//     case 6:
+//     case 7:
+//     case 8:
+//       console.log('лето');
+//       break;
+//     case 9:
+//     case 10:
+//     case 11:
+//       console.log('осень');
+//       break;
+//     default:
+//       console.log('Устал считать');
+//   }
 
 
