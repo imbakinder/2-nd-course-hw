dcbc72a999aa5ae063d24c51726d111204c8cb04
imbakinder <128403500+imbakinder@users.noreply.github.com>
2023-04-02 18:43:25 +0300

    hw -2.4

--- script.js
+++ script.js
@@ -1,60 +1,38 @@
-let password = "пароль";
-let userPass = prompt("Введите пароль");
+// let i = 1;
 
-userPass = userPass.toLocaleLowerCase();
-
-if (userPass === password) {
-  console.log("Пароль введен верно");
-} else {
-  console.log("Пароль введен не верно");
-}
-
-// let c = Number(prompt("Введите число"));
-
-// if (c > 0 && c < 10) {
-//   console.log("Верно");
-// } else {
-//   console.log("Не верно");
+// while (i < 2) {
+//   console.log('Привет');
+//   i++
 // }
 
-// let d = Number(prompt("Введите первое число"));
-// let e = Number(prompt("Введиет второе число"));
 
-// if (d > 100 || e > 100) {
-//   console.log("верно");
-// } else {
-//   console.log("не верно");
+// let i = 1;
+
+// while (i < 5) {
+//   console.log(i);
+//   i++
 // }
 
-// let a = Number("2");
-// let b = Number("3");
-// alert(a + b);
 
-// let monthNumber = String(prompt("Какой месяц?"));
+// let i = 7;
 
-// let monthNumber = Number(prompt('Какой месяц?'));
+// while (i < 23) {
+//   console.log(i);
+//   i++
+// }
 
-// switch (monthNumber) {
-//     case 12:
-//     case 1:
-//     case 2:
-//       console.log('зима');
-//       break;
-//     case 3:
-//     case 4:
-//     case 5:
-//       console.log('весна');
-//       break;
-//     case 6:
-//     case 7:
-//     case 8:
-//       console.log('лето');
-//       break;
-//     case 9:
-//     case 10:
-//     case 11:
-//       console.log('осень');
-//       break;
-//     default:
-//       console.log('Устал считать');
-//   }
+
+// let obj = {'Коля': 200, 'Вася': 300, 'Петя': 400}
+
+// for(key in obj) {
+//     console.log([key]+ ' - зарплата'+  obj[key] +'долларов');
+// }
+
+
+// for (let n = 1000; n >= 50; n++) {
+//     n = n/2;
+//     alert(n);
+    
+// }
+
+
