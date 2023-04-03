bd68d2a29b795173a9a19c3209a6e0209bc46672
imbakinder <128403500+imbakinder@users.noreply.github.com>
2023-04-03 15:07:16 +0300

    Доработка HW-2.4

--- script.js
+++ script.js
@@ -25,7 +25,7 @@
 // let obj = {'Коля': 200, 'Вася': 300, 'Петя': 400}
 
 // for(key in obj) {
-//     console.log([key]+ ' - зарплата'+  obj[key] +'долларов');
+//     console.log([key]+ ' - зарплата '+  obj[key] +' долларов');
 // }
 
 
@@ -36,3 +36,16 @@
 // }
 
 
+
+
+// const firstFriday = 7; 
+// const daysInMonth = 30; 
+
+
+// for (let day = firstFriday; day <= daysInMonth ; day += 7) {
+  
+//   const message = `Сегодня пятница, ${day}-е число. Необходимо подготовить отчет.`;
+  
+  
+//   console.log(message);
+// }      
