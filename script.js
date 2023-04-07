8c2abc8ecd67d14064a98920f2644810cdf1ec85
imbakinder <128403500+imbakinder@users.noreply.github.com>
2023-04-07 15:13:42 +0300

    HW-2.5

--- script.js
+++ script.js
@@ -69,5 +69,34 @@
 // console.log(cubeNumber('a')); 
 
 
+// const circle1 = {
+//     radius: 5,
+//     getArea() {
+//       return Math.PI * this.radius ** 2;
+//     },
+//     getPerimeter() {
+//       return 2 * Math.PI * this.radius;
+//     }
+//   };
+  
+//   const circle2 = {
+//     radius: 10,
+//     getArea() {
+//       return Math.PI * this.radius ** 2;
+//     },
+//     getPerimeter() {
+//       return 2 * Math.PI * this.radius;
+//     }
+//   };
+
+
+// console.log( circle1.getArea() );
+// console.log( circle1.getPerimeter() );
+
+// console.log( circle2.getArea() );
+// console.log( circle2.getPerimeter() );
+
+
+
 
 
