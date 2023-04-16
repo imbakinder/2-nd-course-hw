6bda1254590a6261b7dab848d721f9452dfb955a
Oleg <grammy1488@gmail.com>
2023-04-16 21:58:33 +0300

    GAME-2

--- script.js
+++ script.js
@@ -1,111 +1,78 @@
-// const number = [1, 5, 4, 10, 0, 3];
+// let str = "js";
+// let upperStr = str.toUpperCase();
 
-// for (let i = 0; i < number.length; i++) {
-//         if (number[i] === 10) {
-//             console.log(number[i]);
-//             break
-//         }
-//         console.log(number[i]);
-//     }
+// console.log(upperStr);
 
-// const number = [1, 5, 4, 10, 0, 3];
-// const index = number.indexOf(4);
-// console.log(index);
+// let num = 32.58884;
+// let roundedDown = Math.floor(num);
+// console.log(roundedDown);
+// let num = 32.58884;
+// let roundedUp = Math.ceil(num);
+// console.log(roundedUp);
+// let num = 32.58884;
+// let rounded = Math.round(num);
+// console.log(rounded);
 
-// let number = [1, 3, 5, 10, 20];
-// number = number.join(' ');
-// console.log(number);
+// let arr = [52, 53, 49, 77, 21, 32];
 
-// let arr = [];
+// let min = arr[0];
+// let max = arr[0];
 
-// for (let i = 0; i < 3; i++) {
-//   let innerArr = [];
-//   for (let j = 0; j < 3; j++) {
-//     innerArr.push(1);
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] < min) {
+//     min = arr[i];
 //   }
-//   arr.push(innerArr);
-// }
-// console.log(arr);
 
-// let arr = [1, 1, 1];
-// arr.push(1, 2, 3);
-// console.log(arr);
-
-// const arr = [9, 8, 7, "a", 6, 5];
-// arr.sort();
-// arr.splice(arr.indexOf("a"), 1);
-// const newArr = arr.filter((item) => typeof item === "number");
-
-// console.log(newArr);
-
-// const arr = [9, 8, 7, 6, 5];
-// const num = parseInt(prompt("Введите число от 1 до 10"));
-// const isIncluded = arr.includes(num);
-
-// if (isIncluded) {
-//   console.log(`Число ${num} содержится в массиве`);
-// } else {
-//   console.log(`Число ${num} не содержится в массиве`);
+//   if (arr[i] > max) {
+//     max = arr[i];
+//   }
 // }
 
-// let str = 'abdef';
-// console.log(str.split('').reverse().join(''));
+// console.log('Наименьшее число: ', min);
+// console.log('Наибольшее число: ', max);
 
-// const arr = [
-//   [1, 2, 3],
-//   [4, 5, 6],
-// ];
-// const newArr = arr.flat();
+// function getRandomNumber() {
+//     let randomNumber = Math.random() * 10;
 
-// console.log(newArr);
-
-// let arr = [1, 4, 5, 7, 9, 10, 3, 6, 2, 8];
-
-// for (let i = 0; i < arr.length; i++) {
-//   console.log(arr[i] + arr[i - 1]);
-// }
-
-
-// function squareArray(arr) {
-//     const result = arr.map((item) => item * item);
-//     return result;
+//     randomNumber = Math.floor(randomNumber) + 1;
+//     console.log(randomNumber);
 //   }
-  
-//   const numbers = [2, 3, 4, 5];
-//   const squaredNumbers = squareArray(numbers);
-  
-//   console.log(squaredNumbers);
 
+//   getRandomNumber();
 
-// function getLengthWords(arr) {
-//     const result = [];
-//     for (let i = 0; i < arr.length; i++) {
-//       result.push(arr[i].length);
-//     }
-//     return result;
-//   }
-  
-//   const words = ['слово', '', 'слог', 'длинное предложение', 'буква'];
-//   const wordsLength = getLengthWords(words);
-  
-//   console.log(wordsLength);
+// function getRandomNumbersArray(num) {
+//     let arrayLength = Math.floor(num / 2);
 
+//     let resultArray = [];
 
-// function getNegativeNumbers(arr) {
-//     const result = [];
-//     for (let i = 0; i < arr.length; i++) {
-//       if (arr[i] < 0) {
-//         result.push(arr[i]);
+//     for (let i = 0; i < arrayLength; i++) {
+//       let randomNumber = Math.floor(Math.random() * num);
+
+//       if (!resultArray.includes(randomNumber)) {
+//         resultArray.push(randomNumber);
 //       }
 //     }
-//     return result;
+
+//     return resultArray;
 //   }
-  
-//   const numbers = [1, -2, 3, -4, 5, -6];
-//   const negativeNumbers = getNegativeNumbers(numbers);
-  
-//   console.log(negativeNumbers);  - 
 
+//   console.log(getRandomNumbersArray(10));
 
+// Math.floor(Math.random() * (max - min + 1)) + min
 
+// function getRandomInt(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+//   }
 
+//   console.log(getRandomInt(10, 20));
+
+// const currentDate = new Date();
+// const dateAsString = currentDate.toLocaleDateString();
+// console.log(dateAsString);
+
+// const currentDate = new Date();
+// currentDate.setDate(currentDate.getDate() + 73);
+// console.log(currentDate.toLocaleDateString());
+
+// const date = new Date();
+// console.log(formatDate(date));
